0f819b16fb518adcdcb5b39147c79b8ee02cc086
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2020-08-03 18:12:10 -0300

    Colocando exercicios no toc e transformando em admonitions

--- material/translate-admonitions.js
+++ material/translate-admonitions.js
@@ -11,22 +11,30 @@ let en_pt = {
 let admonitions_to_translate = document.querySelectorAll(".admonition-title");
 for (var i = 0; i < admonitions_to_translate.length; i++) {
     let el = admonitions_to_translate[i];
-    
+
     if (el.innerText in en_pt) {
         el.innerText = en_pt[el.innerText];
     }
 }
 
+/* Substituindo tags */
+let admonition_titles = document.querySelectorAll(".admonition h3");
+admonition_titles.forEach(function (title) {
+    titleClasses = title.getAttribute("class");
+    if (titleClasses == null) titleClasses = "";
+    titleClasses += " admonition-title";
+    title.setAttribute("class", titleClasses);
+});
 
 /* Contagem automática de tarefas e perguntas */
-let counters = ["example", "question"];
-counters.forEach(function (admo_type) {
-    let elements = document.querySelectorAll(".admonition." + admo_type + " .admonition-title");
-    for (let i = 0; i < elements.length; i++) {
-        let el = elements[i];
-        el.innerText += " " + (i+1);
-    }    
-});
+// let counters = ["example", "question"];
+// counters.forEach(function (admo_type) {
+//     let elements = document.querySelectorAll(".admonition." + admo_type + " .admonition-title");
+//     for (let i = 0; i < elements.length; i++) {
+//         let el = elements[i];
+//         el.innerText += " " + (i+1);
+//     }
+// });
 
 
 // Adicionar embed dos slides se existir
@@ -35,19 +43,19 @@ nome_aula = nome_aula.split("/");
 nome_aula = nome_aula[nome_aula.length - 2];
 nome_aula = site_url + "aulas/" + nome_aula + "-slides.pdf";
 let xhr = new XMLHttpRequest();
-xhr.addEventListener("load", function(ev) {
-    if (xhr.status == 200) {       
+xhr.addEventListener("load", function (ev) {
+    if (xhr.status == 200) {
         let emb = document.createElement("embed");
         emb.src = nome_aula;
         emb.width = 500;
         emb.height = 375;
         emb.type = "application/pdf";
-        
+
         let div_center = document.createElement("div");
         div_center.style = "text-align: center";
         div_center.className = "no-print";
         div_center.appendChild(emb);
-        
+
         document.querySelector("article.md-typeset h1").insertAdjacentElement("afterend", div_center);
     }
 });
